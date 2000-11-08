40d61efa2996fd8eb935673c416eee03f421a081
tomas <tomas>
2000-11-08 16:52:19 +0000

    Took the smarter and more verbose version from Zen.

--- autogen.sh
+++ autogen.sh
@@ -29,28 +29,68 @@ FILE=src/tos.c
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
 }
 
-test $TEST_TYPE $FILE || {
-	echo "You must run this script in the top-level $PROJECT directory"
-	exit 1
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoheader installed to compile $PROJECT."
+	echo "It is included in the autoconf package, available from"
+	echo "your distribution, or from ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
+if test ! $TEST_TYPE $FILE
+then
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+fi
+
+if test "x$DIE" != "x"
+then
+    exit 1;
+fi
+
+if test -d /usr/local/share/aclocal && test ! -h /usr/local/share/aclocal
+then
+    ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
+fi
+
+echo -n "Running aclocal with options '$ACLOCAL_FLAGS'... "
 aclocal $ACLOCAL_FLAGS
+if test $? -ne 0; then echo; echo "aclocal failed."; exit 1; fi
+echo "done"
+
+echo -n "Running libtoolize... "
 libtoolize --force
+if test $? -ne 0; then echo; echo "libtoolize failed."; exit 1; fi
+echo "done"
 
 # optionally feature autoheader
-(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+echo -n "Running autoheader... "
+autoheader
+if test $? -ne 0; then echo; echo "autoheader failed."; exit 1; fi
+echo "done"
 
+echo -n "Running automake... "
 automake -a
+if test $? -ne 0; then echo; echo "automake failed."; exit 1; fi
+echo "done"
+
+echo -n "Running autoconf... "
 autoconf
+if test $? -ne 0; then echo; echo "autoconf failed."; exit 1; fi
+echo "done"
 
 cd $ORIGDIR
 
 $srcdir/configure "$@"
+if test $? -ne 0; then echo; echo "configure failed."; exit 1; fi
 
-echo 
-echo "Now type 'make' to compile $PROJECT."
+if test $? -eq 0
+then
+    echo 
+    echo "Now type 'make' to compile $PROJECT."
+fi
