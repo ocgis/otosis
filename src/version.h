a630d7c3522e13ded1e997820291f6d5c6b9443c
cg <cg>
2001-04-03 06:49:08 +0000

    - Another forgotten file

---
+++ src/version.h
@@ -0,0 +1,30 @@
+/************************************************************************
+ *
+ *  oTOSis - TOS emulator for Linux/68K
+ *
+ *  Copyright 2001 Christer Gustavsson <cg@nocrew.org>
+ *
+ *  version.h
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ ************************************************************************/
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+void print_version(void);
+
+#endif /* __VERSION_H__ */
